23333f0dcdfbc9be6564ac8c8d78bf158a298010
mBrossois <markdeneutbrossois@Marks-MacBook-Pro.local>
2023-08-10 08:29:24 +0200

    Added contentful wiht getting the two first calls

--- src/index.js
+++ src/index.js
@@ -1,8 +1,47 @@
+import * as contentful from 'contentful'
+
 const hamburger = document.getElementById('hamburger')
 const nav = document.querySelector('nav')
+
+// Contentful
+const client = contentful.createClient({
+    space: import.meta.env.VITE_CONTENTFUL_SPACE,
+    environment: import.meta.env.VITE_CONTENTFUL_ENVIRONMENT,
+    accessToken: import.meta.env.VITE_CONTENTFUL_ACCESSTOKEN,
+})
+
+const navigation = await client
+  .getEntries({
+    content_type: 'navigation',
+    limit: '1'
+})
+
+const pagesResult = await client
+    .getEntries({
+        content_type: 'page'
+})
+
+console.log(pagesResult)
+
+const pages = pagesResult.items.map(item => {
+    return item.fields
+})
+
+console.log(pages)
+  
+for(let navItem of navigation.items[0].fields.items) {
+    const link = document.createElement("a")
+
+    link.textContent=navItem
+    link.setAttribute('href', `#${navItem.toLowerCase()}`)
+    nav.appendChild(link)
+}
+
 const navELements = document.querySelectorAll('nav a')
 let active = navELements[0]
 active.classList.add('active')
+
+
 
 // Event listeners
 hamburger.addEventListener('click', () => {
@@ -29,3 +68,4 @@ for(let element of navELements) {
         active = event.target
     })
 }
+
